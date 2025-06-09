2791a7d59895aed4b7af75821d65be87327b565d
aatieh_42 <amjdsalh@gmail.com>
2025-06-09 21:04:44 +0300

    Module_06 ex02 done

---
+++ Module_06/ex02/main.cpp
@@ -0,0 +1,98 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aatieh <aatieh@student.42amman.com>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/09 17:57:40 by aatieh            #+#    #+#             */
+/*   Updated: 2025/06/09 21:04:02 by aatieh           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+#include <iostream>
+#include <stdlib.h>
+#include <ctime>
+
+Base * generate(void)
+{
+	Base*	P;
+
+	std::srand(std::time(NULL));
+	if (rand() % 3 == 0)
+		P = new A();
+	else if (rand() % 2 == 0)
+		P = new B();
+	else
+		P = new C();
+	return P;
+}
+
+void identify(Base* p)
+{
+	if (dynamic_cast<A*>(p))
+		std::cout << "Class A" << std::endl;
+	else if (dynamic_cast<B*>(p))
+		std::cout << "Class B" << std::endl;
+	else if (dynamic_cast<C*>(p))
+		std::cout << "Class C" << std::endl;
+	else
+		std::cout << "Unknown class" << std::endl;
+}
+
+void identify(Base& p)
+{
+	Base *d = &p;
+	if (dynamic_cast<A*>(d))
+		std::cout << "Class A" << std::endl;
+	else if (dynamic_cast<B*>(d))
+		std::cout << "Class B" << std::endl;
+	else if (dynamic_cast<C*>(d))
+		std::cout << "Class C" << std::endl;
+	else
+		std::cout << "Unknown class" << std::endl;
+}
+
+int main()
+{
+	std::cout << "---- Test 1: Single generate call ----" << std::endl;
+	// Base *p = generate();
+	std::cout << "Generating instances of A, B, and C:" << std::endl;
+	Base *p = new A();
+	std::cout << "identify(p): ";
+	identify(p);
+	std::cout << "identify(*p): ";
+	identify(*p);
+	delete p;
+
+	p = new B();
+	std::cout << "identify(p): ";
+	identify(p);
+	std::cout << "identify(*p): ";
+	identify(*p);
+	delete p;
+
+	p = new C();
+	std::cout << "identify(p): ";
+	identify(p);
+	std::cout << "identify(*p): ";
+	identify(*p);
+	delete p;
+
+	std::cout << "\n---- Test 2: Multiple generate calls ----" << std::endl;
+	for (int i = 0; i < 10; ++i)
+	{
+		Base *p2 = generate();
+		std::cout << "Test " << i + 1 << " - identify(p2): ";
+		identify(p2);
+		std::cout << "Test " << i + 1 << " - identify(*p2): ";
+		identify(*p2);
+		delete p2;
+	}
+
+	std::cout << "\nAll tests completed." << std::endl;
+	return 0;
+}
